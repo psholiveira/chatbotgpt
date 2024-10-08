0258620ae9e8f124c1ff790f3eacf1801578f72b
Pedro Santos <160976446+psholiveira@users.noreply.github.com>
2024-10-08 17:39:41 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,13 +3,23 @@ import openai
 chave_api = 'sk-20dIjcJ4Z8-wVP2ltowVYy4XPHQ1ygxTPxxOfJhpSMT3BlbkFJF4XyCgdCseyAzGgz-6hcsycyJ2YTuv5WVYsmg77CEA'
 openai.api_key = chave_api
 
-def enviar_mensagem(mensagem):
+def enviar_mensagem(mensagem, lista_mensagens=[]):
+    lista_mensagens.append(
+        {"role": "user", "content": mensagem}
+    )
+   
     resposta = openai.Completion.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": mensagem},
-        ]
+        messages = lista_mensagens, 
     )
     return resposta["choices"][0]["message"]
 
-print(enviar_mensagem("MESSAGE"))
+lista_mensagem = []
+while True:
+    texto = input("Escreva aqui sua mensagem:")
+    if texto == "sair":
+        break
+    else:
+        reposta = enviar_mensagem(texto, lista_mensagem)
+        lista_mensagem.append(resposta)
+        print("Chatbot:", reposta["content"])
